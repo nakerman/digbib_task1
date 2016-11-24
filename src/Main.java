c74d13c3bd353332e731d43bcf6caaabb6921d8b
Florian Weißensteiner <florian.weissensteiner@student.tugraz.at>
2016-11-24 18:53:07 +0100

    multi keyword search and snazzy console input. jar to check it out included

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,9 @@ import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
-import org.apache.lucene.util.BytesRef;
 import org.apache.pdfbox.cos.COSDocument;
 import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDDocumentInformation;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.pdfbox.io.RandomAccessFile;
 
@@ -21,25 +19,104 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main{
+
+    static StandardAnalyzer analyzer = null;
+    static Directory ramDirectory = null;
+
     public static void main(String[] args) {
-        if(args.length != 2) {
-            System.out.println("Usage: 'java -jar <programname> <dir> <keyword");
-            System.exit(2);
-        }
+        String DIR = ".";
+        String SEARCHSTRING = "";
 
-        final String DIR = args[0];
-        final String KEYWORD = args[1];
+        System.out.println("- Please enter the directory containing the PDFs.");
+        System.out.println("- The directory containing the jar-file is \".\" so a subdirectory would be accessed as \"./subdir\"");
+        System.out.print("> ");
 
-
-        // EXTRACT TEXT:
-
-        ArrayList<File> files = new ArrayList<File>();
-        ArrayList<String> documentsTitles = new ArrayList<String>();
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter(Pattern.compile("[\\n]"));
         ArrayList<String> documentsText = new ArrayList<String>();
 
-        getFilesInDirectory(files, DIR);
+        DIR = scanner.next();
+
+        File folder = new File(DIR);
+        while(folder.listFiles() == null || documentsText.size() == 0) {
+            if(folder.listFiles() == null) {
+                System.out.println("- The directory \"" + DIR + "\" does not exist, please enter a valid directory. (did you forget the dot?)");
+                System.out.print("> ");
+            }
+            else {
+                System.out.println("");
+                System.out.println("- Indexing PDFs. This may take some time...");
+
+                // EXTRACT TEXT:
+                documentsText = extractTextFromPdfs(DIR);
+
+                if(documentsText.size() != 0) break;
+
+                System.out.println("- The directory \"" + DIR + "\" does not contain any PDFs, please enter a valid directory.");
+                System.out.print("> ");
+            }
+
+            scanner = new Scanner(System.in);
+            DIR = scanner.next();
+            folder = new File(DIR);
+        }
+
+
+
+        //INDEX
+        indexPdfs(documentsText);
+
+        System.out.println("- Finished Indexing PDFs.");
+        System.out.println("");
+
+        System.out.println("- Please enter the term(s) you would like to search for. You may use AND and/or OR as keywords.");
+        System.out.println("- Examples: \"apple\"; \"apple AND pear\"; \"(apple OR pear) AND sauce\".");
+        System.out.print("> ");
+
+        while(true) {
+            SEARCHSTRING = scanner.next();
+            if(SEARCHSTRING.equals("diglib quit")) break;
+
+            TopDocs topDocs = null;
+            try {
+                //SEARCH
+                topDocs = searchPdfs(SEARCHSTRING, 10);
+            }
+            catch(ParseException ex) {
+                System.out.println("");
+                System.out.println("- The search-string you entered contains invalid syntax. Please enter a valid search-string.");
+                System.out.print("> ");
+                continue;
+            }
+            ScoreDoc[] scoreDocs = topDocs.scoreDocs;
+
+            System.out.println("- A total of " + topDocs.totalHits + " PDFs matched your query.");
+            System.out.println("- The top results are:");
+
+            for(int id = 0; id < scoreDocs.length; id++) {
+                System.out.println("- Number " + scoreDocs[id].doc + " with a score of: " + scoreDocs[id].score);
+            }
+
+            System.out.println("");
+            System.out.println("- Please enter the term(s) for your next search. You may quit anytime by entering \"diglib quit\".");
+            System.out.print("> ");
+        }
+    }
+
+    /**
+     * @param directory directory containing the pdfs (subdirectories are searched aswel)
+     * @return list containing the text of the pdfs
+     */
+    private static ArrayList<String> extractTextFromPdfs(String directory)
+    {
+        ArrayList<String> documentsText = new ArrayList<String>();
+        ArrayList<File> files = new ArrayList<File>();
+
+        getFilesInDirectory(files, directory);
 
         try {
             PDFTextStripper stripper = new PDFTextStripper();
@@ -53,10 +130,8 @@ public class Main{
 
                 COSDocument cosDocument = parser.getDocument();
                 PDDocument pdDocument = new PDDocument(cosDocument);
-                PDDocumentInformation info = pdDocument.getDocumentInformation();
 
                 String documentText = stripper.getText(pdDocument);
-                documentsTitles.add(info.getTitle());
                 documentsText.add(documentText);
                 pdDocument.close();
             }
@@ -65,68 +140,13 @@ public class Main{
             ex.printStackTrace();
         }
 
-
-        // INDEX AND SEARCH
-
-        try {
-
-            // INDEX
-
-            StandardAnalyzer analyzer = new StandardAnalyzer();
-            Directory directory = new RAMDirectory();
-            IndexWriterConfig config = new IndexWriterConfig(analyzer);
-            IndexWriter indexWriter = new IndexWriter(directory, config);
-
-            for(int i = 0; i < documentsText.size(); i++)
-            {
-                String documentText = documentsText.get(i);
-                String documentTitle = documentsTitles.get(i);
-
-                FieldType fieldType = new FieldType(TextField.TYPE_STORED);
-                fieldType.setStoreTermVectors(true);
-
-                Document document = new Document();
-                document.add(new Field("content", documentText, fieldType));
-                indexWriter.addDocument(document);
-            }
-
-            indexWriter.close();
-
-            // SEARCH
-            Query query = new QueryParser("content", analyzer).parse(KEYWORD);
-
-            int numberTopDocs = 1;
-            IndexReader reader = DirectoryReader.open(directory);
-            IndexSearcher searcher = new IndexSearcher(reader);
-            TopDocs topDocs = searcher.search(query, numberTopDocs);
-            System.out.println("Found " + topDocs.totalHits + " hits.");
-
-            /*
-            ScoreDoc[] scoreDocs = topDocs.scoreDocs;
-            for(int i = 0; i < scoreDocs.length; i++) {
-                Terms termVector = reader.getTermVector(scoreDocs[i].doc, "content");
-                TermsEnum itr = termVector.iterator();
-                BytesRef term = null;
-
-                while((term = itr.next()) != null) {
-                    String termText = term.utf8ToString();
-                    Term termInstance = new Term("contents", term);
-                    long termFreq = reader.totalTermFreq(termInstance);
-                    long docCount = reader.docFreq(termInstance);
-
-                    System.out.println("term: " + termText + ", termFreq = " + termFreq + ", docCount = " + docCount);
-                }
-            }
-            */
-        }
-        catch(IOException ex) {
-            ex.printStackTrace();
-        }
-        catch(ParseException ex) {
-            ex.printStackTrace();
-        }
+        return documentsText;
     }
 
+    /**
+     * @param files found pdfs
+     * @param dirName directory to be searched in (subdirectories are also searched)
+     */
     public static void getFilesInDirectory(ArrayList<File> files, String dirName) {
         File folder = new File(dirName);
         ArrayList<File> folderFiles = new ArrayList<File>(Arrays.asList(folder.listFiles()));
@@ -139,5 +159,55 @@ public class Main{
                 getFilesInDirectory(files, dirName + "/" + file.getName());
             }
         }
+    }
+
+    /**
+     * @param documentsText list containing the text of the pdfs
+     */
+    private static void indexPdfs(ArrayList<String> documentsText) {
+        try {
+            analyzer = new StandardAnalyzer();
+            ramDirectory = new RAMDirectory();
+            IndexWriterConfig config = new IndexWriterConfig(analyzer);
+            IndexWriter indexWriter = new IndexWriter(ramDirectory, config);
+
+            for(int i = 0; i < documentsText.size(); i++)
+            {
+                String documentText = documentsText.get(i);
+
+                FieldType fieldType = new FieldType(TextField.TYPE_STORED);
+                fieldType.setStoreTermVectors(true);
+
+                Document document = new Document();
+                document.add(new Field("content", documentText, fieldType));
+                indexWriter.addDocument(document);
+            }
+
+            indexWriter.close();
+        }
+        catch(IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * @param searchString e.g.: "(apple OR pear) AND sauce"
+     * @param numberTopDocs number of top results to be returned
+     * @return pdfs with top results
+     */
+    private static TopDocs searchPdfs(String searchString, int numberTopDocs) throws ParseException {
+        TopDocs topDocs = null;
+        try {
+            Query query = new QueryParser("content", analyzer).parse(searchString);
+
+            IndexReader reader = DirectoryReader.open(ramDirectory);
+            IndexSearcher searcher = new IndexSearcher(reader);
+            topDocs = searcher.search(query, numberTopDocs);
+        }
+        catch(IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return topDocs;
     }
 }
